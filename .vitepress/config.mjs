ee83c5ba2ab1e5c302a9d69375c5c0a9f5bae89d
piaorongzhen <prz_2022@qq.com>
2024-12-26 15:36:01 +0800

    配置文件添加base配置

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+    base: '/docs/',
     title: "RZ-Link Docs",
     description: "A VitePress Site",
     themeConfig: {
